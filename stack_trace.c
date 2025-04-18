534cb86322e16c1b8fb29e45f7eecacdb6f10125
OGUlab-git <ogulab2035@gmail.com>
2025-04-18 14:09:28 +0900

    Create stack_trace.c

---
+++ stack_trace.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+ 
+	void test_function(int a,int b,int c,int d){
+		int flag;
+		char buffer[10];
+	
+		flag = 12345;
+		buffer[0] = 'A';
+	}
+	
+	int main(){
+		test_function(1,2,3,4);
+	}
